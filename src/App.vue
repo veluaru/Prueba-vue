3b637d798db8da919370ce1435f5fe7ae60690ca
veluaru <angie.castaneda@udea.edu.co>
2020-09-15 23:10:33 -0500

    prueba 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      color="indigo darken-2"
+      dark
+      shrink-on-scroll
+      scroll-target="#scrolling-techniques"
+      class="shrink"
+    >
+      <v-toolbar-title>Mi Galeria de Imágenes</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <HelloWorld v-bind:images="images" />
+      <ImageList />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImageList from "./components/ImageList";
+import HelloWorld from "./components/HelloWorld";
+//import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+    ImageList,
+  },
+  data() {
+    return {
+      images: [
+        {
+          id: 1,
+          text: "mmmmm",
+        },
+        {
+          id: 2,
+          text: "asd",
+        },
+      ],
+    };
+  },
+  // methods: {
+  // beforeCreate() {
+  //   console.log("holi");
+  //   axios
+  //     .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+  //     .then((response) => (this.images = response))
+  //     .catch((err) => console.log(err));
+  // },
+  // },
+};
+</script>
---
+++ src/components/ImageList.vue
@@ -0,0 +1,23 @@
+<template>
+<v-container>
+    <div v-bind:key="image.id" v-for="image in images">
+        <h1>holi {{image.text}}</h1>
+    </div>
+    <h1>holi</h1>
+     <v-btn small color="primary" v-on:click="print()">Primary</v-btn>
+
+</v-container>
+</template>
+
+<script>
+export default {
+    name: 'ImageList',
+    props:["images"],
+    methods:{
+        print() {
+            console.log(this.images)
+        }
+    }
+}
+
+</script>
